ac5cf5f8ec00c5d2fa11763454fa12a6474b1566
Pavel Netolický <pavel.netolicky@gmail.com>
2016-12-11 23:41:37 +0100

    run_all: new proccesing file; TODO: Simple clear version...

--- run_all.py
+++ run_all.py
@@ -48,7 +48,7 @@ def create_vectors_from_file(file_name, input_dir, output_dir, output_format):
 
 def cluster_vector_file(file_path, cluster_count, output_dir):
     os.chdir(output_dir)
-    real_cluto_params = default_cluto_params.format(input_file=file_path, cluster_count=cluster_count, cluto_log='log_' + file_name +'.'+ str(cluster_count))
+    real_cluto_params = default_cluto_params.format(input_file=file_path, cluster_count=cluster_count, cluto_log= file_name +'.'+ str(cluster_count)+ '_log')
     run_cmd = '{0} {1}'.format(cluto_path, real_cluto_params)
     ret_code = subprocess.call(run_cmd, shell=True, stdout=FNULL, stderr=FNULL)
     print('return code cluto: {0}'.format(ret_code))
@@ -75,37 +75,50 @@ if __name__ == "__main__":
             for cl_c in cluster_counts:
                 cluster_vector_file(file_path, cl_c, os.path.join(temp_dir, 'vectext_1'))
 
-
-
-    # Tohle asi neni potreba
-    # print('======Replace class 0 with 2 in cluto files======')
-    # files_dir = os.path.join(temp_dir, 'cluto_1')
-    # for file_name in sorted(os.listdir(files_dir)):
-    #     subprocess.call("sed -i -e 's/0/2/g' {0}".format(os.path.join(files_dir, file_name)), shell=True)
-
     print('======Join classes from clusters and articles======')
     cluto_files_dir = os.path.join(temp_dir, 'vectext_1')
     for cluto_file_name in sorted(os.listdir(cluto_files_dir)):
-        if cluto_file_name.endswith('.original.txt'):
-            orig_file = cluto_file_name
-        if cluto_file_name.endswith('.sparse.clustering.2'):
-            sparse_file=cluto_file_name
-        # print('==={0}==='.format(cluto_file_name))
-    # Have no idea how this works :(
-    # You must get all files from cluto_1/ and connect them with files from inputs/ and save the result to cluto_2/
-    # if file_name.endswith('original.txt'):
-    os.chdir(results_dir)
-    run_cmd_sed = "sed -i -e 's/0/2/g' %s" %(sparse_file)
-    ret_code_sed = subprocess.call(run_cmd_sed, shell=True, stdout=FNULL, stderr=FNULL)
-    print('return code sed : {0}'.format(ret_code_sed))
-    run_cmd_dos2unix = 'dos2unix %s| dos2unix %s ' %(orig_file,sparse_file)
-    ret_code_dos2unix = subprocess.call(run_cmd_dos2unix, shell=True, stdout=FNULL, stderr=FNULL)
-    print('return code: {0}'.format(ret_code_dos2unix))
-    class_file = sparse_file+'.classes'
-    run_cmd_cut = "cut -f 2-4  %s > dates; paste -d'\t' %s dates > %s" %(orig_file,sparse_file,class_file)
-    # run_cmd_cut = "paste -d'\t' %s %s > 48_article.classes" %(sparse_file,orig_file)
-    ret_code_cut = subprocess.call(run_cmd_cut, shell=True, stdout=FNULL, stderr=FNULL)
-    print('return code: {0}'.format(ret_code_cut))
+        base_name = cluto_file_name.split('.')[0]
+        print('==={0}==='.format(base_name))
+        orig_file = os.path.join(cluto_files_dir, '{0}.original.txt'.format(base_name)) #input_dir
+        sparse_file = os.path.join(cluto_files_dir, '{0}.sparse.clustering.2'.format(base_name))
+        # Process files
+        os.chdir(results_dir)
+        run_cmd_sed = "sed -i -e 's/0/2/g' %s" %(sparse_file)
+        ret_code_sed = subprocess.call(run_cmd_sed, shell=True, stdout=FNULL, stderr=FNULL)
+        print('return code sed : {0}'.format(ret_code_sed))
+        run_cmd_dos2unix = 'dos2unix %s| dos2unix %s ' %(orig_file,sparse_file)
+        ret_code_dos2unix = subprocess.call(run_cmd_dos2unix, shell=True, stdout=FNULL, stderr=FNULL)
+        print('return code: {0}'.format(ret_code_dos2unix))
+        class_file = sparse_file+'.classes'
+        run_cmd_cut = "cut -f 2-4  %s > dates; paste -d'\t' %s dates > %s" %(orig_file,sparse_file,class_file)
+        # run_cmd_cut = "paste -d'\t' %s %s > 48_article.classes" %(sparse_file,orig_file)
+        ret_code_cut = subprocess.call(run_cmd_cut, shell=True, stdout=FNULL, stderr=FNULL)
+        print('return code: {0}'.format(ret_code_cut))
+
+
+
+
+    # print('======Join classes from clusters and articles======')
+    # cluto_files_dir = os.path.join(temp_dir, 'vectext_1')
+    # for cluto_file_name in sorted(os.listdir(cluto_files_dir)):
+    #     if cluto_file_name.endswith('.original.txt'):
+    #         orig_file = cluto_file_name
+    #     if cluto_file_name.endswith('.sparse.clustering.2'):
+    #         sparse_file=cluto_file_name
+    #
+    # os.chdir(results_dir)
+    # run_cmd_sed = "sed -i -e 's/0/2/g' %s" %(sparse_file)
+    # ret_code_sed = subprocess.call(run_cmd_sed, shell=True, stdout=FNULL, stderr=FNULL)
+    # print('return code sed : {0}'.format(ret_code_sed))
+    # run_cmd_dos2unix = 'dos2unix %s| dos2unix %s ' %(orig_file,sparse_file)
+    # ret_code_dos2unix = subprocess.call(run_cmd_dos2unix, shell=True, stdout=FNULL, stderr=FNULL)
+    # print('return code: {0}'.format(ret_code_dos2unix))
+    # class_file = sparse_file+'.classes'
+    # run_cmd_cut = "cut -f 2-4  %s > dates; paste -d'\t' %s dates > %s" %(orig_file,sparse_file,class_file)
+    # # run_cmd_cut = "paste -d'\t' %s %s > 48_article.classes" %(sparse_file,orig_file)
+    # ret_code_cut = subprocess.call(run_cmd_cut, shell=True, stdout=FNULL, stderr=FNULL)
+    # print('return code: {0}'.format(ret_code_cut))
 
     # # run_cmd_sed = "sed -i -e 's/0/2/g' %s" %(class_file)
     # ret_code_sed = subprocess.call(run_cmd_sed, shell=True, stdout=FNULL, stderr=FNULL)
